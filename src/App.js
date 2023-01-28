011394c400e270791a7586de421df6d6e6422796
LeonardoAlmds <leonardoalmd8@outlook.com>
2023-01-27 22:54:36 -0300

    cansei

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/authContext";
 import { Signup } from "./pages/Signup";
 
 function App() {
   return (
     <AuthProvider>
-      <Signup />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signup'>
+            
+          </Route>
+            <Signup />
+        </Routes>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
